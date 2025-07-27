5406b5445061743c94273f14a3eea6e07dbf3695
insotheo <maxim.insotheo@gmail.com>
2025-07-27 19:28:24 +0400

    Project setup

---
+++ MINServer/Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace MINServer.Controllers
+{
+    [Route("[controller]")]
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return PhysicalFile(
+                Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "hello", "index.html"),
+                "text/html"
+                );
+        }
+    }
+}
---
+++ MINServer/Startup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace MINServer
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSignalR();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            //add db later
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.Use(async (context, next) =>
+            {
+                if(context.Request.Path == "/")
+                {
+                    context.Response.Redirect("/hello");
+                    return;
+                }
+                await next();
+            });
+
+            app.UseRouting();
+
+            //add auth later
+            //app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+    }
+}
