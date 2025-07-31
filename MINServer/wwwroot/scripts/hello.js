171f4329de4fdf87a2ade0b11b22e64b210bef89
insotheo <maxim.insotheo@gmail.com>
2025-07-31 15:26:12 +0400

    hello.js

---
+++ MINServer/wwwroot/scripts/hello.js
@@ -0,0 +1,12 @@
+﻿function toggleForms(e) {
+    e.preventDefault();
+    document.getElementById('login-container').classList.toggle('active');
+    document.getElementById('register-container').classList.toggle('active');
+}
+
+//theme
+const toggleButton = document.getElementById('theme-toggle');
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('light-theme');
+    toggleButton.textContent = document.body.classList.contains('light-theme') ? '🌞' : '🌙';
+});
